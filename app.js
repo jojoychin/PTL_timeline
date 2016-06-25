b52e012715e9a23be425d03b023bf6f017e6259c
jojoychin <jochin@Joannas-MacBook-Air.local>
2016-06-25 14:30:06 -0400

    hopefully adding stuff

---
+++ app.js
@@ -0,0 +1,132 @@
+//JS in-class 3/3/2015
+
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var port = 9000;
+
+app.use('/', express.static(__dirname + '/public'));
+server.listen(port, function() {
+    console.log('Server running at port:' + port);
+});
+
+var users = [];
+
+var aiArray = {
+    1: {
+        "name":'AI bot',
+        "msg": 'Great. Started workspace for _. Who do you want to invite to attend the meeting?',
+        "state": 1
+    },
+    2: {
+        "name":'AI bot',
+        "msg": 'I\'ve added _ to the meeting. What is the date and time that the meeting will take place?',
+        "state": 2
+    },
+    3: {
+        "name":'AI bot',
+        "msg":'Got it. Sending a calendar invite for _.',
+        "state": 3
+    }
+};
+
+//.on = listener function (for an event)
+//everything on the server happens in .on scope
+io.on('connection', function(socket) {
+    /*––––––––––– SOCKET.IO starts here –––––––––––––––*/
+
+    /*
+	.on
+	.emit
+	.broadcast
+
+    */
+    //logging user id
+    console.log('The user '+ socket.id+' just connected.');
+
+    users.push(socket.id);
+    console.log('current users: '+users.length);
+
+    //when a client connects to server, broadcast to everyone
+    io.sockets.emit('current users', {
+    	//attaching whole array (users) in currentUsers object
+    	currentUsers: users
+    });
+
+    var state = 0;
+
+    //listens for user disconnection
+    socket.on('disconnect', function(){
+    	console.log('a user '+socket.id+ ' just disconnected.');
+    	//use indexOf to find index of res.id
+    	var indexToRemove = users.indexOf(socket.id);
+
+    	if(indexToRemove > -1){
+    	//indexToRemove will return index number of contect provided
+    	//or -1 if not found
+    	//second arg is for how many indexes to remove
+    	users.splice(indexToRemove, 1);
+    	console.log('current users: '+ users.length);
+    	}
+    });
+
+    // socket.emit('greetings', "hey there. this is server speaking...");
+
+    //need to send back id to cliet bc only the server knows it
+    socket.emit('greetings', {
+    	msg: "Hi",
+    	data: socket.id,
+        state: 0
+    });
+
+    socket.on('message', function(res){
+    	console.log(res.state);
+        //call parseResponse function
+        parseState(res);
+        parseRes(res);
+    	//broadcast to all
+    	io.sockets.emit('clients', {
+    		data: res
+    	})
+    	//broadcast to one
+    	// socket.emit('confirmed', {
+    	// 	confirm: res.message_real
+    	// });
+    });
+
+    var parseState = function(userRes) {
+        //change state for first scenario
+        state+=1;
+        userRes.state = state;
+        console.log("current user state: " + userRes.state);
+        chooseBotRes(userRes);
+    }
+
+    var parseRes = function(userRes) {
+        //parse reseponse for other parts of the meeting
+    }
+
+    var chooseBotRes = function(res) {
+        setTimeout(function() {
+        var currentRes = aiArray[res.state];
+        // if (res.state == 2){
+            console.log('got to state 2 with '+res.msg);
+            var altRes = currentRes.msg.replace('_', res.msg);
+            // currentRes.msg = altRes;
+            // console.log(currentRes.msg);
+            io.sockets.emit('botRes', {
+            msg: altRes,
+            name: currentRes.name,
+            state: currentRes.state
+        })
+        // } else {
+        // console.log('this is currentRes: '+currentRes.msg);
+        // io.sockets.emit('botRes', {
+        //     data: currentRes
+        // })
+    // }
+    }, 2000);
+}
+
+});
---
+++ public/js/main.js
@@ -0,0 +1,145 @@
+/* Your code starts here */
+
+var app = {};
+
+app.init = function() {
+    var socket;
+    var name;
+
+    var start = function() {
+        //create a name input using prompt
+        //first is the prompt, second arg is the in the input
+        name = prompt('What is your name?', 'type your name here');
+        //init socket with server
+        //meaning connect to the server
+        //don't need to put anything in io.connect() bc
+        //server is hosting everything incl socket
+        socket = io.connect();
+
+        socket.on('greetings', function(res) {
+            console.log(res);
+            startMsg(res, name);
+        });
+
+        socket.on('confirm', function(res) {
+            console.log(res);
+        });
+
+        socket.on('current users', function(res) {
+            console.log('current users: '+res);
+        });
+
+        socket.on('broadcast message', function(res) {
+            console.log(res);
+        });
+
+        attachEvents();
+
+    };
+
+    var attachEvents = function() {
+        var state = 0;
+        //keeping every eventlistener 
+        $('#js-btn-send').on('click', function() {
+            //after user clicks
+            var chat_msg = $('#js-ipt-text').val();
+            console.log(chat_msg);
+            var re = /<script\b[^>]*>([\s\S]*?)<\/script>/gm;
+            var isScript;
+            isScript = re.test(chat_msg);
+            //.emit submits or sends an event
+            //you can name it anything and you can
+            //attach anything (string, num etc.)
+            //but usually it's an object
+            socket.emit('message', {
+                // message_one: "hey lovely.",
+                // message_two: "hola!",
+                user: name,
+                msg: chat_msg,
+                state: state
+            });
+            // Reset input field
+            $('#js-ipt-text').val('');
+        });
+
+        $('#chat-input').keyup(function(event){
+            if(event.keyCode == 13){
+             var chat_msg = $('#js-ipt-text').val();
+             console.log('enter pressed & value is: '+chat_msg);
+             var re = /<script\b[^>]*>([\s\S]*?)<\/script>/gm;
+            var isScript;
+            isScript = re.test(chat_msg);
+            console.log(isScript);
+
+            console.log('Sending: ' + chat_msg);
+            // emit a chat message to server
+            socket.emit('message', {
+                name: name,
+                msg: chat_msg,
+                state: state
+            });
+            // Reset input field
+            $('#js-ipt-text').val('');
+            }
+        });
+
+    //mousemove event test
+    // $(window).on('mousemove', function(res){
+    //   //could also do pageX, pageY; sometimes one works
+    //   //better than the other
+    //   var posX = res.clientX;
+    //   var posY = res.clientY;
+
+    //   console.log(posX, posY);
+
+    //   socket.emit('mouse position', {
+    //     x: posX,
+    //     y: posY
+    //   })
+    // });
+
+    //listen for clients
+    socket.on('clients', function(res) {
+        console.log('this is the res: '+res);
+        var tplToCompile = $('#tpl-chat-item').html();
+        var compiled = _.template(tplToCompile)({
+            timestamp: 'now',
+            data: res.data
+        });
+        $('#chat-container').prepend(compiled);
+        console.log(res.data);
+    });
+
+    socket.on('botRes', function(res) {
+        var tplToCompile = $('#tpl-bot-item').html();
+        var compiled = _.template(tplToCompile)({
+            timestamp: 'now',
+            msg: res.msg,
+            name: res.name,
+            state: res.state
+        });
+        
+            $('#chat-container').prepend(compiled);
+        console.log(res.data);
+    });
+ };
+
+var startMsg = function(response, name){
+    var data = {'current':0,'name': 'AI Bot', 'msg': 'Hi, '+name+'. What would you like to name this meeting?'};
+    // console.log('start message is: '+response.msg + name);
+    // var greeting = response.msg+', '+ name+'. What would you like to name this meeting?';
+    var tplToCompile = $('#tpl-bot-item').html();
+    var compiled = _.template(tplToCompile)({
+        timestamp: 'now',
+        msg: data.msg,
+        name: data.name,
+        state: data.state
+        // message: greeting
+    });
+    $('#chat-container').prepend(compiled);
+}
+
+start();
+};
+
+app.init();
